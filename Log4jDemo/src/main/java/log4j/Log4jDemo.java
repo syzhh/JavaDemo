0daa741c7a36e0ae13eb3edbec295feb31b5b55a
das2017 <38510430@qq.com>
2018-10-07 00:37:18 +0800

    updated log4j demo

--- Log4jDemo/src/main/java/log4j/Log4jDemo.java
+++ Log4jDemo/src/main/java/log4j/Log4jDemo.java
@@ -14,10 +14,7 @@ public class Log4jDemo {
     private static final Logger logger = LogManager.getLogger("RollingRandomAccessFileLogger");
 
     public static void main(String[] args) throws IOException {
-        File file = new File("src/main/resources/log4j2.xml");
-        BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
-        final ConfigurationSource source = new ConfigurationSource(in);
-        Configurator.initialize(null, source);
+        loadConfig();
 
         for(int i = 0; i < 50000; i++) {
             logger.trace("trace日志");
@@ -28,17 +25,14 @@ public class Log4jDemo {
             logger.fatal("致命错误的日志", new Exception("发生了一个致命错误"));
         }
 
-        try {
-            Thread.sleep(1000 * 61);
-        } catch (InterruptedException e) {}
-
-        logger.trace("trace日志");
-        logger.debug("调试信息的日志");
-        logger.info("一般信息的日志");
-        logger.warn("警告信息的日志");
-        logger.error("错误信息的日志", new Exception("发生了一个异常"));
-        logger.fatal("致命错误的日志", new Exception("发生了一个致命错误"));
-
         System.out.println("日志记录完毕。");
+    }
+
+    private static void loadConfig() throws IOException
+    {
+        File file = new File("src/main/resources/log4j2.xml");
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+        final ConfigurationSource source = new ConfigurationSource(in);
+        Configurator.initialize(null, source);
     }
 }
